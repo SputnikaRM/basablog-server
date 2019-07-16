b98964e0a235c3c310f47fc963ed57a55f5f4cf3
benrybinski <benrybin@gmail.com>
2019-07-16 14:47:52 -0400

    fixed small issue

--- src/main/java/com/unzip/unzip/Repositories/CommentsRepository.java
+++ src/main/java/com/unzip/unzip/Repositories/CommentsRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class CommentsRepository extends CrudRepository <Comments,Long> {
+public interface CommentsRepository extends CrudRepository <Comments,Long> {
 }
--- src/main/java/com/unzip/unzip/Repositories/UserRepository.java
+++ src/main/java/com/unzip/unzip/Repositories/UserRepository.java
@@ -3,5 +3,5 @@ package com.unzip.unzip.Repositories;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class UserRepository {
+public interface UserRepository {
 }
