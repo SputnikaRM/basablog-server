64eb64f7dde8d18c5cceeff49766978f040820ea
sputnika <sputnika@zipcoders-MacBook-Pro-5.local>
2019-07-17 14:39:00 -0400

    addblog,post

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BloglistComponent } from './bloglist/bloglist.component';
+import { AddblogComponent } from './addblog/addblog.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path: '',
+  component: BloglistComponent
+},
+
+{
+  path: 'addblog',
+  component: AddblogComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
