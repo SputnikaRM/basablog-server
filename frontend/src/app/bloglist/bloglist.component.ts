e8c1a17272dbf354531ad7d641869a68fa3f5e3f
benrybinski <benrybin@gmail.com>
2019-07-17 11:13:05 -0400

    progress

--- frontend/src/app/bloglist/bloglist.component.ts
+++ frontend/src/app/bloglist/bloglist.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import {Post} from 'src/app/post';
+import {Comment} from 'src/app/comment';
 
 @Component({
   selector: 'app-bloglist',
@@ -7,13 +10,24 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./bloglist.component.css']
 })
 export class BloglistComponent implements OnInit {
-  private blogposturl = "http://localhost:8080/";
-  private blog
+  private blogposturl = "http://localhost:8080/blogPost/add";
+  private bloggeturl =  "http://localhost:8080/blogPost/all";
+  private blogdeleteurl = "http://localhost:8080/";
+  private blogupdateurl = "http://localhost:8080/";
+  private commentgeturl = "http://localhost:8080/";
+  private commentposturl = "http://localhost:8080/";
 
   constructor(private http:HttpClient) {
-
+    
     
    }
+   public findAllBlogPost(): Observable<Post[]>{
+     return this.http.get<Post[]>(this.bloggeturl);
+   }
+   public save(post: Post){
+     return this.http.post<Post>(this.blogposturl,post);
+   }
+   
 
   ngOnInit() {
   }
