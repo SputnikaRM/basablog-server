6659f91943c49d373058441ec19944e4d9265896
sputnika <sputnika@zipcoders-MacBook-Pro-5.local>
2019-08-01 08:29:27 -0400

    update

--- src/main/java/com/unzip/unzip/Models/BlogPost.java
+++ src/main/java/com/unzip/unzip/Models/BlogPost.java
@@ -13,6 +13,9 @@ public class BlogPost {
     private String postText;
     private String imageURL;
     private String tag1;
+    private String tag2;
+    private String tag3;
+
 
 
 
@@ -82,7 +85,21 @@ public class BlogPost {
         this.tag1 = tag1;
     }
 
+    public String getTag2() {
+        return tag2;
+    }
 
+    public void setTag2(String tag2) {
+        this.tag2 = tag2;
+    }
+
+    public String getTag3() {
+        return tag3;
+    }
+
+    public void setTag3(String tag3) {
+        this.tag3 = tag3;
+    }
 }
 
 
